0ec1095e7085827a3ee281acde1220b37dd64d19
yoon764 <yoonji22704@gmail.com>
2024-11-11 23:48:10 +0900

    Update Enemy.h

--- SWProject/Enemy.h
+++ SWProject/Enemy.h
@@ -8,11 +8,23 @@ public:
 	~Enemy();
 
 	void Init();
-	void Update();
-	void Render();
 
-	// _ ���̱�
+	void Update();
+	int DetectCollision();//적 유닛 충돌 여부 검사 함수
+	void Move();//적 유닛 이동 함수
+	void Attack();//적 유닛 공격 함수
+	void ChangeHp(int hp);//적 유닛의 체력 변경 함수
+	void Destroy();//적 유닛 체력 < 0 일 경우, 소멸하는 함수
+
+	void Render();//시각화
+	void Show();//정보맵에서의 수정(등록)
+	void Hide();//정보맵에서의 수정(삭제)
+	
+	int GetHp();
+	int GetMaxHp();
+	Pos GetPos();
+	ObjectType GetObjectType();
+
 private:
 
 };
-
